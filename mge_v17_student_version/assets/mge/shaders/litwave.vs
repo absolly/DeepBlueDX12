72e48c383f8715175540eea9e49fcbb6784fd3fc
Tiemen van Egmond <absolly.epsilon@gmail.com>
2017-02-09 16:22:56 +0100

    Animation working

---
+++ mge_v17_student_version/assets/mge/shaders/litwave.vs
@@ -0,0 +1,81 @@
+//DIFFUSE TEXTURE VERTEX SHADER
+#version 330 // for glsl version (12 is for older versions , say opengl 2.1
+
+uniform sampler2D textureWaveMask;
+uniform	mat4 	projectionMatrix;
+uniform	mat4 	viewMatrix;
+uniform	mat4 	modelMatrix;
+uniform vec3    lightPosition[24];
+uniform vec3    lightDirection[24];
+uniform int     lightType[24];
+uniform	int     lightCount;
+uniform float	_time;
+in vec3			vertex;
+in vec3			normal;
+in vec2			uv;
+in vec3			tangent;
+in vec3			bitangent;
+
+
+
+out vec2 texCoord; //make sure the texture coord is interpolated
+out vec3 Position_worldspace;
+mat3 TBN;
+out vec3 LightDirection_tangentspace[24];
+out vec3 EyeDirection_tangentspace;
+vec3 LightDirection_cameraspace[24];
+vec3 EyeDirection_cameraspace;
+
+
+void main( void ) {
+	vec3 waveSize = vec3(texture(textureWaveMask, uv));
+	waveSize *= 100;
+
+
+	vec3 newvertex = vertex + vec3(waveSize.r * cos((_time*0.02)  + (vertex.x * 0.001)), waveSize.g * cos((_time*0.02) + (vertex.x * 0.001)),waveSize.b * cos((_time*0.02) + (vertex.z * 0.001)));
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(newvertex, 1.f);
+    texCoord = uv;
+
+    vec3 vertexNormal_cameraspace = (viewMatrix * modelMatrix * vec4(normalize(normal),0)).xyz;
+    vec3 vertexTangent_cameraspace = (viewMatrix * modelMatrix * vec4(normalize(tangent),0)).xyz;
+    vec3 vertexBitangent_cameraspace = (viewMatrix * modelMatrix * vec4(normalize(bitangent),0)).xyz;
+
+    TBN = transpose(mat3(
+                        vertexTangent_cameraspace,
+                        vertexBitangent_cameraspace,
+                        vertexNormal_cameraspace
+                    ));
+
+    // Position of the vertex, in worldspace : modelMatrix * position
+    Position_worldspace = (modelMatrix * vec4(vertex,1)).xyz;
+
+    // Vector that goes from the vertex to the camera, in camera space.
+    // In camera space, the camera is at the origin.
+    vec3 vertexPosition_cameraspace = ( viewMatrix * modelMatrix * vec4(vertex,1)).xyz;
+    EyeDirection_cameraspace = vec3(0,0,0) - vertexPosition_cameraspace;
+    EyeDirection_tangentspace =  TBN * EyeDirection_cameraspace;
+
+    for(int activeLight = 0; activeLight < lightCount; activeLight++) {
+        // Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity).
+        vec3 LightPosition_cameraspace = ( viewMatrix * vec4(lightPosition[activeLight],1)).xyz;
+        //LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
+
+        switch(lightType[activeLight]) {
+        case 0:
+            LightDirection_cameraspace[activeLight] = LightPosition_cameraspace + EyeDirection_cameraspace;
+            break;
+        case 1:
+            LightDirection_cameraspace[activeLight] =  (viewMatrix * vec4(lightDirection[activeLight],0)).xyz;
+            break;
+        case 2:
+            LightDirection_cameraspace[activeLight] = LightPosition_cameraspace + EyeDirection_cameraspace;
+            break;
+        }
+
+        LightDirection_tangentspace[activeLight] = TBN * LightDirection_cameraspace[activeLight];
+    }
+    // Normal of the the vertex, in camera space
+    //vec3 Normal_cameraspace = ( viewMatrix * modelMatrix * vec4(normal,0)).xyz; // Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+
+
+}
--- mge_v17_student_version/src/Content/TestScene.cpp
+++ mge_v17_student_version/src/Content/TestScene.cpp
@@ -18,6 +18,7 @@ using namespace std;
 #include "mge/materials/ColorMaterial.hpp"
 #include "mge/materials/TextureMaterial.hpp"
 #include "mge/materials/WobbleMaterial.hpp"
+#include "mge/materials/LitWaveMaterial.hpp"
 
 #include "mge/behaviours/RotatingBehaviour.hpp"
 #include "mge/behaviours/RigidBody.hpp"
@@ -60,10 +61,10 @@ void TestScene::_initializeScene() {
     //load a bunch of meshes we will be using throughout this demo
     //each mesh only has to be loaded once, but can be used multiple times:
     //F is flat shaded, S is smooth shaded (normals aligned or not), check the models folder!
-    Mesh* planeMeshDefault = Mesh::load (config::MGE_MODEL_PATH+"plane.obj");
+    Mesh* planeMeshDefault = Mesh::load (config::MGE_MODEL_PATH+"Creature_OBJ.obj");
     Mesh* cubeMeshF = Mesh::load (config::MGE_MODEL_PATH+"cube_flat.obj");
     Mesh* suzannaMeshF = Mesh::load (config::MGE_MODEL_PATH+"suzanna_smooth.obj");
-    Mesh* teapotMeshS = Mesh::load (config::MGE_MODEL_PATH+"teapot_smooth.obj");
+    Mesh* teapotMeshS = Mesh::load (config::MGE_MODEL_PATH+"teapotMeshS.obj");
     // Mesh* carMesh = Mesh::load(config::MGE_MODEL_PATH+"car.obj");
     //MATERIALS
 
@@ -74,15 +75,17 @@ void TestScene::_initializeScene() {
 
     //10 specular teapot material
     AbstractMaterial* textureMaterial2 = new TextureMaterial (Texture::load (config::MGE_TEXTURE_PATH+"bricks.jpg"), 1, 10);
+	AbstractMaterial* waveMaterial = new LitWaveMaterial(Texture::load(config::MGE_TEXTURE_PATH + "bricks.jpg"), Texture::load(config::MGE_TEXTURE_PATH + "CreatureUV_GRN.png"), 1, 10);
 
     //SCENE SETUP
     PhysicsObject* plane = new PhysicsObject ("plane", glm::vec3(0,0,0));
-    plane->scale(glm::vec3(50,50,50));
+    //plane->scale(glm::vec3(50,50,50));
     plane->setMesh(planeMeshDefault);
-    plane->setMaterial(textureMaterial);
+    plane->setMaterial(waveMaterial);
+	plane->addBehaviour(new KeysBehaviour());
 
     btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-    groundShape->setLocalScaling(btVector3(50,50,50));
+    //groundShape->setLocalScaling(btVector3(50,50,50));
 
     btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
     btRigidBody::btRigidBodyConstructionInfo
@@ -93,9 +96,9 @@ void TestScene::_initializeScene() {
 
     GameObject* teapot = new GameObject ("teapot", glm::vec3(-3,1,0));
     teapot->setMesh (teapotMeshS);
+	teapot->scale(glm::vec3(0.1,0.1,0.1));
     teapot->setMaterial(textureMaterial2);
     teapot->addBehaviour (new KeysBehaviour());
-
     _world->add(teapot);
 
 //    for(int i = 0; i < 1000; i++){
@@ -110,29 +113,29 @@ void TestScene::_initializeScene() {
 //    car->setMesh (carMesh);
 //    car->setMaterial(colorMaterial);
 //    _world->add(car);
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
+   // for(int i = 0; i < 10; i++) {
+   //     for(int j = 0; j < 10; j++) {
 
-				btCollisionShape* fallShape = new btSphereShape(1);
-				btDefaultMotionState* fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0 + i, 50 + i, 0 + i)));
-				btScalar mass = 1;
-				btVector3 fallInertia(0, 0, 0);
-				fallShape->calculateLocalInertia(mass, fallInertia);
-				btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
+			//	btCollisionShape* fallShape = new btSphereShape(1);
+			//	btDefaultMotionState* fallMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0 + i, 50 + i, 0 + i)));
+			//	btScalar mass = 1;
+			//	btVector3 fallInertia(0, 0, 0);
+			//	fallShape->calculateLocalInertia(mass, fallInertia);
+			//	btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, fallMotionState, fallShape, fallInertia);
 
-            GameObject* monkey = new GameObject ("monkey", glm::vec3(3,1,0));
-            monkey->setMesh (suzannaMeshF);
-            monkey->setMaterial(textureMaterial2);
-			monkey->addBehaviour(new RigidBody(fallRigidBodyCI));
-      //    monkey->setBehaviour (new RotatingBehaviour());
+   //         GameObject* monkey = new GameObject ("monkey", glm::vec3(3,1,0));
+   //         monkey->setMesh (suzannaMeshF);
+   //         monkey->setMaterial(textureMaterial2);
+			//monkey->addBehaviour(new RigidBody(fallRigidBodyCI));
+   //   //    monkey->setBehaviour (new RotatingBehaviour());
 
-            _world->add(monkey);
-        }
-    }
-	Player* player = new Player();
-	_world->add(player);
-	player->add(camera);
-    //camera->addBehaviour(new CameraOrbitBehaviour (10, 30, 150, 1, teapot));
+   //         _world->add(monkey);
+   //     }
+   // }
+	//Player* player = new Player();
+	//_world->add(player);
+	//player->add(camera);
+    camera->addBehaviour(new CameraOrbitBehaviour (10, 30, 150, 1, teapot));
 //
 //    glm::vec3* lightColor = new glm::vec3(0.5f,0.0f,.5f);
 //    Light* light = new Light (Light::lightType::POINT, "light1", glm::vec3(0,2,-5), *lightColor, 50, glm::vec3(0,0,1));
@@ -151,7 +154,7 @@ void TestScene::_initializeScene() {
     float random = time(NULL);
     std:: cout << "random seed: " << random << std::endl;
     srand (random);
-    for(int i = 0; i < 24; i++) {
+    for(int i = 0; i < 23; i++) {
         glm::vec3* lightColor = new glm::vec3(rand() % 100,rand() % 100,rand() % 100);
         Light* light = new Light (Light::lightType::POINT, "light1", glm::vec3(rand() % 100 - 50,5,rand() % 100 - 50), *lightColor, 50, glm::vec3(0,0,1));
         light->setMesh (cubeMeshF);
@@ -160,6 +163,13 @@ void TestScene::_initializeScene() {
         light->setMaterial(colorMaterial2);
         _world->add(light);
     }
+	glm::vec3* lightColor = new glm::vec3(1,1,1);
+	Light* light = new Light(Light::lightType::DIRECTIONAL, "light1", glm::vec3(0,100,0), *lightColor, 1, glm::vec3(0, 0, 1));
+	light->setMesh(cubeMeshF);
+	AbstractMaterial* colorMaterial2 = new ColorMaterial(glm::normalize(*lightColor));
+	light->addBehaviour(new LookAt(teapot));
+	light->setMaterial(colorMaterial2);
+	_world->add(light);
 
 //    Light* light3 = new Light (Light::lightType::POINT, "light3", glm::vec3(10,2,-10), *lightColor, 100.f, Light::lightFalloff::CONSTANT);
 //    light3->setMesh (cubeMeshF);
