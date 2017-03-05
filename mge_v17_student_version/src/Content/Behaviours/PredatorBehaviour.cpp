444b19c788bfe263c83b9e0d43a29db7d6691b5c
Tiemen van Egmond <absolly.epsilon@gmail.com>
2017-03-05 12:39:14 +0100

    Change debug material for predator crumbs

--- mge_v17_student_version/src/Content/Behaviours/PredatorBehaviour.cpp
+++ mge_v17_student_version/src/Content/Behaviours/PredatorBehaviour.cpp
@@ -1,23 +1,21 @@
 #include "PredatorBehaviour.h"
 #include "mge/core/GameObject.hpp"
 #include "mge/config.hpp"
-#include "mge/materials/TextureMaterial.hpp"
 #include "mge/core/Physics/PhysicsWorld.h"
 #include "mge\core\World.hpp"
 #include "Content/Core/Input.h"
 PredatorBehaviour::PredatorBehaviour(GameObject * pTarget, std::vector<glm::vec3> pWaypoints, World * pWorld) : _waypoints(pWaypoints), _target(pTarget), _world(pWorld)
 {
 	Mesh* cubeMeshF = Mesh::load(Config::MGE_MODEL_PATH + "cube_flat.obj");
-	AbstractMaterial* textureMaterial = new TextureMaterial(Texture::load(Config::MGE_TEXTURE_PATH + "bricks.jpg"));
+	AbstractMaterial* colorMaterial2 = new ColorMaterial((glm::vec3(0.1, 1, 0.1)));
 	for (int i = 0; i < 16; i++) {
 		_crumbObjects[i] = new GameObject("", glm::vec3(0, 0, 0));
 		_crumbObjects[i]->setMesh(cubeMeshF);
-		_crumbObjects[i]->setMaterial(textureMaterial);
+		_crumbObjects[i]->setMaterial(colorMaterial2);
 		_crumbObjects[i]->scale(glm::vec3(0.1f));
 		pWorld->add(_crumbObjects[i]);
 		_crumbObjects[i]->setParent(NULL);
 	}
-	AbstractMaterial* colorMaterial2 = new ColorMaterial((glm::vec3(0.1,1,0.1)));
 	for each(glm::vec3 waypoint in _waypoints) {
 		GameObject* go = new GameObject("", waypoint);
 		go->setMesh(cubeMeshF);
